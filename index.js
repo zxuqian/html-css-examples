d20f910d753369543fc58772bc719a6595af3925
Phill Weston <2436559745@qq.com>
2024-05-01 10:58:21 +0800

    Add day-night-switch-button (#16)
    by @Phillweston

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const uis = [{
+  href: "52-day-night-toggle-button",
+  text: "白天黑夜切换按钮",
+  newItem: true
+}, {
   href: "51-css-scroll-based-animation",
   text: "CSS Scroll Based Animation",
   newItem: true
